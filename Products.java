07cbfe0f185fb6b1068b11b6aa6b9633f746c58b
Roiczone <roi.kenne.24@lehre.mosbach.dhbw.de>
2025-09-24 17:20:30 +0200

    just created the constructors for my products class, so Daniel can use them in the main class

--- Products.java
+++ Products.java
@@ -1,2 +1,43 @@
 public class Products {
+
+    public static class Product {
+        private int id;
+        private String name;
+        private double price;
+        private int stock;
+        private String category;
+
+        public Product(int id, String name, double price, int stock, String category) {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.stock = stock;
+            this.category = category;
+        }
+
+        public int getId() {return id;}
+
+        public String getName() {return name;}
+
+        public double getPrice() {return price;}
+
+        public int getStock() {return stock;}
+
+        public String getCategory() {return category;}
+
+        public void setStock(int stock) {this.stock = stock;}
+
+        @Override
+        public String toString() {
+            return id + " | " + name + " | $" + price + " | Stock: " + stock + " | " + category;
+        }
+
+        public String toJson() {
+            return String.format(
+                    "{\"id\":%d,\"name\":\"%s\",\"price\":%.2f,\"stock\":%d,\"category\":\"%s\"}",
+                    id, name, price, stock, category
+            );
+        }
+
+    }
 }
